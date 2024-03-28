d593ddeb745c288c7fabb819cf808f1cbc87b6a2
muneeb12344 <143326416+muneeb12344@users.noreply.github.com>
2024-03-28 23:23:10 +0600

    Update and rename main.js to bonus.js

---
+++ bonus.js
@@ -0,0 +1,4 @@
+//bonus.moneygift.shop
+var price = 494;
+var upi = "successfully727372@sbi";
+console.log(price);
--- main.js
+++
@@ -1,4 +0,0 @@
-  //main.moneygift.shop
-        var price = 494;
-        var upi = "successfully727372@sbi";
-        console.log(price);
